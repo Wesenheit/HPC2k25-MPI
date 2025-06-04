e80bea1530b06bcebdfc1833eb6ac5e33a4895fe
Mateusz Kapusta <mitra@Voyager-2.local>
2025-06-04 23:45:51 +0200

    heavy light

--- src/Node_baseline.cpp
+++ src/Node_baseline.cpp
@@ -4,19 +4,20 @@ void Node::run()
 {
     int k = 0;
     int work_to_do;
+    std::vector<bool> was_deleted(N,false);
     do
     {
         while (k < buckets.size() && (!buckets[k] || buckets[k]->empty()))
             k++;
-        
+
         if (k >= buckets.size())
             k = MAX;
 
         k = all_reduce(&k,MPI_MIN);
-        
+
         if (k == MAX)
             break;
-
+        std::vector<int> deleted;
         do
         {
             if (k < buckets.size() && buckets[k])
@@ -26,13 +27,19 @@ void Node::run()
                 {
                     int u = bucket.front();
                     bucket.pop_front();
+                    if (!was_deleted[u])
+                    {
+                        was_deleted[u] = true;
+                        deleted.push_back(u);
+                    }
                     for (int i = 0; i < distances.size();i++)
                     {
                         if (vertex_1[i] == u)
                         {
                             int v = vertex_2[i];
                             int d = distances[i];
-                            relax(u,v,d + tenative[u-lower]);
+                            if (d <= Delta)
+                                relax(u,v,d + tenative[u-lower]);
                         }
                     }
 
@@ -43,6 +50,20 @@ void Node::run()
         }
         while (all_reduce(&work_to_do,MPI_LOR));
 
+        //Heavy reduction
+        for (auto vertex:deleted)
+        {
+            for (int i = 0; i < distances.size();i++)
+            {
+                if (vertex_1[i] == vertex)
+                {
+                    int v = vertex_2[i];
+                    int d = distances[i];
+                    if (d > Delta)
+                        relax(vertex,v,d + tenative[vertex-lower]);
+                }
+            }
+        }
     k++;
     }
     while (true);
